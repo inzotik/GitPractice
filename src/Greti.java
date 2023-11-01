9966314e9cb5f5ac52d9c84d35ad547f8d5bbf86
Inna_Cydeo <innazotikova@gmail.com>
2023-11-01 13:27:47 -0500

    greti inna branch

--- src/Greti.java
+++ src/Greti.java
@@ -1,5 +1,5 @@
 public class Greti {
     public static void main(String[] args) {
-        System.out.println("someone is playing around");
+        System.out.println("actually, someone is playing around");
     }
 }
