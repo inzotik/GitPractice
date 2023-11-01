e99952963132a5a5ebfa5e3c37bc73de9cc59b7e
Inna_Cydeo <innazotikova@gmail.com>
2023-11-01 13:28:43 -0500

    greti master branch

--- src/Greti.java
+++ src/Greti.java
@@ -1,5 +1,5 @@
 public class Greti {
     public static void main(String[] args) {
-        System.out.println("no one is playing around");
+        System.out.println("no, you don't understand, no one is playing around");
     }
 }
